299e9df61d711e038ab97b2cd59d17dab45e6dfb
unknown <rodrigo09.mesquita@gmail.com>
2021-06-01 11:23:40 -0300

    Projeto API CRUD + MongoDB Atlas

---
+++ Company-API/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_API.Controllers
+{
+    [Route("Controller")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly CompanyServices _companyServices;
+        public CompanyController(CompanyServices companyServices)
+        {
+            _companyServices = companyServices;
+        }
+    [HttpGet]
+        public ActionResult<List<Company>> Get() =>
+            _companyServices.Get();
+        public ActionResult<Company> Get(string id)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # API-Crud-e-MongoDB-
 API Crud DotNet 6.0 com Mongo DB
+
+# Projeto API Crud.
+
+- Realizado com DotNet 6.0
+- Integração com Banco de Dados.
+- Aplicação Crud empresas.
+
+# Metodos utilizados.
+
+- GET
+- PUT
+- DELETE
+- POST
+  
+# Banco de Dados.
+
+Mongo DB ATlas.
